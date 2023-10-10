c92645949bdf74aa9621a0cc87f14182f697cea3
hp <sabhrwalkanika1999@gmail.com>
2023-10-10 13:56:03 +0530

    Add project files.

---
+++ Non-Generic Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_Generic_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push("Kanika");
+            s.Push("Sabharwal");
+            s.Push("Welcome!");
+
+            Console.WriteLine(s.Peek());
+            s.Pop();
+            Console.WriteLine(s.Contains("Kanika"));
+            Console.WriteLine(s.Count);
+
+            foreach (var x in s) Console.WriteLine(x);
+
+            Console.ReadLine();
+
+        }
+    }
+}
